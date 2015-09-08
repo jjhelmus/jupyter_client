f24aaf670c061863a34e9450c93879333adc7c18
Jonathan Helmus <jjhelmus@gmail.com>
2015-09-08 15:59:55 -0500

    Remove multikernel

--- jupyter_client/__init__.py
+++ jupyter_client/__init__.py
@@ -1,9 +1,8 @@
 """Client-side implementations of the Jupyter protocol"""
 
-from ._version import version_info, __version__, protocol_version_info, protocol_version
+from ._version import protocol_version_info, protocol_version
 from .connect import *
 from .launcher import *
 from .client import KernelClient
 from .manager import KernelManager, run_kernel
 from .blocking import BlockingKernelClient
-from .multikernelmanager import MultiKernelManager
